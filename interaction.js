b1ff2b70929ad63a0b4525f64baa227c6390c91b
Klius <Kliusescarlopini@gmail.com>
2015-11-17 17:58:04 +0100

    Added div info+style

---
+++ interaction.js
@@ -0,0 +1,23 @@
+window.onload=function(){
+		addMouseOvers();
+	};
+	function addMouseOvers(){
+		var areas = document.getElementsByTagName("area");
+		for(var i = 0; i < areas.length; i++){
+			areas[i].onmouseover = function(){
+				resaltarMapa(this);
+			};
+		}
+	}
+	function resaltarMapa(area){
+		console.log(area)
+		anchors = document.getElementsByTagName("a");
+		for(var i = 0; i < anchors.length ; i++){
+			if(area.alt === anchors[i].id){
+				anchors[i].className="selected";
+			}
+			else{
+				anchors[i].className="";
+			}
+		}
+	}
