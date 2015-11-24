87992ad307dccd37c4c437634d2306f5e77df853
Klius <Kliusescarlopini@gmail.com>
2015-11-24 10:13:08 +0100

    Version 1 finished!

--- interaction.js
+++ interaction.js
@@ -21,8 +21,56 @@ window.onload=function(){
 			}
 		}
 		seleccionarCiudad(area.alt);
+		getInfo(area.alt);
 	}
 	function seleccionarCiudad(classe){
 		document.getElementById("selectedtown").className=classe;
+	}
+	/*Ajax request to get town information*/
+	function getInfo(townName)
+	{
+		var xhttp = new XMLHttpRequest();
+  		xhttp.onreadystatechange = function(){
+		    if (xhttp.readyState == 4 && xhttp.status == 200) 
+		    {
+		      setTownDescription(xhttp.responseText);
+		    }
+	  	};
+		xhttp.open("GET", "assets/towninfo/"+townName+".js", true);
+		xhttp.send();
+	}
 
+
+	function setTownDescription(info){
+		var town = JSON.parse(info);
+		var div = document.getElementById("information");
+		div.innerHTML="";//clear the div
+		
+		var title = getElement("h4",town.name);
+
+		var description = getElement("i",town.description);
+
+		var assetList = document.createElement("ul");
+		assetList = addElementToList(assetList, "Pokemon Center", town.pokemonCenter);
+		assetList = addElementToList(assetList, "Shop", town.shop);
+		if(town.gim !=="none"){
+			assetList = addElementToList(assetList, "Gim", town.gim);
+			assetList = addElementToList(assetList, "Gim Leader", town.gimLeader);
+			assetList = addElementToList(assetList, "Gim Type", town.gimType);
+		}
+		div.appendChild(title);
+		div.appendChild(description);
+		div.appendChild(assetList);
+	}
+
+	function getElement(elementType,text){
+		var element = document.createElement(elementType);
+		element.innerHTML = text;
+		return element;
+	}
+	function addElementToList(list,descriptionText,text){
+		var li = document.createElement("li");
+		li.innerHTML = descriptionText+": "+text;
+		list.appendChild(li);
+		return list;
 	}
